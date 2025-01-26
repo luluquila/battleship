5669daba337db759b72f159f4ba139056eaa1016
Luiza Teles <luizamgteles@gmail.com>
2025-01-26 15:11:46 -0500

    commented out GameOver

--- src/components/MyRoot.jsx
+++ src/components/MyRoot.jsx
@@ -70,7 +70,7 @@ const onCellClick = (row, col) => {
     <div className="my-root">
       <Header clickCount={clickCount} />
       <TargetGrid onCellClick={onCellClick} gridCells={gridCells} />
-      <GameOver />
+      {/* <GameOver /> */}
       <FleetContainer boatFleet={boatFleet} />
     </div>
   );
